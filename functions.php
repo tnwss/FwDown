9f317445d6fba416398461e3e4364d0b60cfa14c
yimoliunian <591628382@qq.com>
2023-07-11 08:50:20 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -41,12 +41,18 @@ function fwdown_scripts() {
 	wp_enqueue_script('login', $url . '/Static/js/login.js', array('jquery'), $version, true);
 	wp_enqueue_script('base', $url . '/Static/js/base.js', array(), $version, true);
 
-	
+	if (is_home()) {
+        wp_dequeue_script('plugin-script'); // 替换 'plugin-script' 为你想要移除的插件脚本的句柄
+    }
 
 	//加载css文件
 	wp_enqueue_style( 'style', $url.'/style.css',array(),$version,false );
 	wp_enqueue_style( 'moblie', $url.'/Static/css/moblie.css',array(),$version,false );
 	
+	  if (is_home()) {
+        wp_dequeue_style('tdvb-td-viewer-style-css'); // 替换 'tdvb-td-viewer-style-css' 为你想要移除的插件样式的句柄
+        wp_deregister_style('tdvb-td-viewer-style-css');
+    }
 	 //登陆用户信息
     if(is_user_logged_in()){
         $logged = 1;
@@ -77,6 +83,13 @@ function fwdown_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'fwdown_scripts' );
 
+function exclude_plugin_css_from_homepage() {
+    if (is_home()) {
+        wp_dequeue_style('tdvb-td-viewer-style'); // 替换 'tdvb-td-viewer-style' 为你想要移除的插件样式的句柄
+        wp_deregister_style('tdvb-td-viewer-style');
+    }
+}
+add_action('wp_enqueue_scripts', 'exclude_plugin_css_from_homepage', 9999);
 
 //后台交互
 require get_template_directory() . '/Admin/functions-admin.php';
