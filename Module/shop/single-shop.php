613bad66c1f4c1a82622e2f1702193c09f46d349
yimoliunian <591628382@qq.com>
2023-07-11 08:49:46 +0800

    Update single-shop.php

--- Module/shop/single-shop.php
+++ Module/shop/single-shop.php
@@ -8,21 +8,34 @@
  */
 get_header();
 ?>
+<style>
+  .newbg {
+    background-size: cover; /* 宽度拉满，保持比例 */
+    background-repeat: no-repeat; /* 不重复 */
+    background-position: center center; /* 图片居中对齐 */
+  }
+</style>
 
-
-<div class="uk-section-default uk-light uk-cover-container uk-section uk-section-xlarge">
-    <div class="uk-container uk-container-expand uk-position-relative">
-                      <?php
-                        while ( have_posts() ) :
-                            the_post();
-                            get_template_part( 'Module/shop/content', get_post_type() );
-                            // If comments are open or we have at least one comment, load up the comment template.
-                            //if ( comments_open() || get_comments_number() ) :comments_template();
-                            //endif;
-                        endwhile; // End of the loop.
-                    ?>
+<div class="newbg uk-light uk-cover-container uk-section uk-section-xlarge" style="background: #317f80;">
+    <div class="uk-position-cover" style="background: #317f80;"></div>
+    <div class="uk-container  uk-container-xlarge  uk-position-relative">
+        <div class="uk-grid tm-grid-expand uk-child-width-1-1 uk-grid-margin">
+           <div class="home-bg">
+        <?php
+            while ( have_posts() ) :
+                the_post();
+                get_template_part( 'Module/shop/content', get_post_type() );
+                // If comments are open or we have at least one comment, load up the comment template.
+                //if ( comments_open() || get_comments_number() ) :comments_template();
+                         //endif;
+            endwhile; // End of the loop.
+        ?>
+        </div>
     </div>
 </div>
+    </div>
+</div>
+
 
 <?php
 get_footer();
